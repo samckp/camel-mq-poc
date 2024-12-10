d456ecea0250011fa7c7d2b8b6f3e6122aeba9a5
samckp <sam.mca.liet@gmail.com>
2024-12-10 18:44:48 +0530

    apache camel SSL connection with kafka.

--- src/main/java/com/camel/kafka/route/KafkaRoute.java
+++ src/main/java/com/camel/kafka/route/KafkaRoute.java
@@ -1,17 +1,50 @@
 package com.camel.kafka.route;
 
+import org.apache.camel.LoggingLevel;
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.stereotype.Component;
 
-//@Component
+import java.util.Date;
+
+@Component
 public class KafkaRoute extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
 
+        from("timer:mytimer?period=1&repeatCount=5")
+                .routeId("producerRoute")
+                .setBody( e -> new Date())
+                .log(LoggingLevel.INFO,"${body}")
+        .to("kafka:mytopic?brokers=localhost:9093"
+                + "&securityProtocol=SSL"
+                + "&sslTruststoreLocation=D:/kafka_2.13-3.9.0/kafka.truststore.jks"
+                + "&sslTruststorePassword="
+                + "&sslKeystoreLocation=D:/kafka_2.13-3.9.0/kafka.keystore.jks"
+                + "&sslKeystorePassword="
+                + "&sslEndpointAlgorithm="
+                + "&sslKeyPassword=password")
+        ;
 
-        from("")
-                .log("")
-                ;
+
+        from("kafka:mytopic?brokers=localhost:9093"
+                + "&securityProtocol=SSL"
+                + "&sslTruststoreLocation=D:/kafka_2.13-3.9.0/kafka.truststore.jks"
+                + "&sslTruststorePassword=password"
+                + "&sslKeystoreLocation=D:/kafka_2.13-3.9.0/kafka.keystore.jks"
+                + "&sslKeystorePassword="
+                + "&sslEndpointAlgorithm="
+                + "&sslKeyPassword="
+                + "&groupId=my-group"
+                + "&autoOffsetReset=earliest"
+                + "&consumersCount=1"
+                + "&keyDeserializer=org.apache.kafka.common.serialization.StringDeserializer"
+                + "&valueDeserializer=org.apache.kafka.common.serialization.StringDeserializer")
+
+                .routeId("consumerRoute")
+                .log("Received message: ${body}")
+                .to("log:received-message")
+        ;
+
     }
 }
